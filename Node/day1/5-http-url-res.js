6a5e4d6848ac1bf769146f6615e0d7138207b6d4
lgmayun <3496294573@qq.com>
2020-03-19 15:23:31 +0800

    add node

---
+++ Node/day1/5-http-url-res.js
@@ -0,0 +1,39 @@
+const http = require('http')
+const Serve = http.createServer()
+Serve.on('request', function (req, res) {
+  let url = req.url
+  // if (url === '/') {
+  //   res.end('node Hello')
+  // } else if (url === '/index') {
+  //   res.end('index')
+  // } else if (url === '/aaa') {
+  //   res.end('saaaa')
+  // } else {
+  //   res.end('Can not found')
+  // }
+  if (url === '/product') {
+    let products = [
+      {
+        name : 'apple',
+        price : 99
+      },
+      {
+        name : 'apple1',
+        price : 199
+      },
+      {
+        name : 'apple2',
+        price : 299
+      },
+      {
+        name : 'apple3',
+        price : 399
+      }
+    ]
+    // 响应内容只能是字符串或者二进制数据其他类型要使用JSON转化
+    res.end(JSON.stringify(products))
+  }
+})
+Serve.listen(3000, function () {
+  console.log('Serve running ...');
+})
