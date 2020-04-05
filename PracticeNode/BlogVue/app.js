ce2fa909a4be65e78c594414ec06940d50e755ae
lgmayun <3496294573@qq.com>
2020-04-05 18:59:06 +0800

    add node

--- PracticeNode/BlogVue/app.js
+++ PracticeNode/BlogVue/app.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const router = require('./routers')
+const session = require('express-session')
+// 该模块用于将session存入mongo中
+const MongoStore = require('connect-mongo')(session)
 const app = express()
 
 
 app.use('/node_modules/', express.static('./node_modules/'))
 app.use('/public/', express.static('./public/'))
-
+app.set('trust proxy', 1) 
+app.use(session({
+  secret: 'keyboard cat',// 配置加密字符串，会在原有基础上和这个字符串拼接起来去加密
+  resave: false,
+  saveUninitialized: true, //无论你是否使用sesion，我都默认直接给你分配一把钥匙
+  cookie:{
+      secure: true,
+      maxAge: 259200000 // 过期时间
+  },
+  store: new MongoStore({
+    url: 'mongodb://localhost/Blog1'
+  })
+}))
 
 // 把路由挂载到app服务中
 app.use(router)
--- PracticeNode/BlogVue/routers.js
+++ PracticeNode/BlogVue/routers.js
@@ -14,23 +14,100 @@ let router = express.Router()
 router.post('/api/admin/signup', urlencodedParser, function (req, res, next) {
   //console.log(req.body);
   req.body.password = md5(md5(req.body.password))
-  //console.log(req.body);
+  //console.log(req.body.username);
   new Db.User(req.body).save(function (err) {
+    if (err) {
+      return next(err)
+      //console.log(err);
+    }
+    res.send()
+  })
+})
+/***
+ * 登录
+ */
+router.post('/api/admin/signin', urlencodedParser,function (req, res, next) {
+  //console.log(req.body);
+  req.body.password = md5(md5(req.body.password))
+  res.send()
+})
+/**
+ * 根据用户名查找用户
+ */
+router.get('/api/admin/getUser/:name', function (req, res) {
+  /**
+   * 使用req.query可以查到指定用户名的所有内容
+   * 但使用req.params却查不到任何内容？？？？
+   *  一眼懵逼，看前端咋做的？？
+   */
+  //console.log(req.params);
+  Db.User.findOne({
+    username: req.params.username
+  })
+})
+/**
+ * 获取所有文章
+ */
+router.get('/api/articleList', function (req, res, next) {
+  console.log(req.query);
+  Db.Article.find(function (err, data) {
+    if (err) {
+      return next(err)
+    }
+    // res.json()的作用的就是就请求的返回值的转化成json的格式
+    res.json(data)
+  })
+})
+/**
+ * 文章详情页
+ */
+router.get('/api/articleDetail/:id', function (req, res, next) {
+  Db.Article.findOne({_id: req.params.id}, function (err, data) {
+    if (err) {
+      return next(err)
+    }
+    res.send(data)
+  })
+})
+/**
+ * 文章保存
+ */
+router.post('/api/admin/saveArticle', urlencodedParser, function (req, res, next) {
+  new Db.Article(req.body.articleInformation).save(function (err) {
     if (err) {
       return next(err)
     }
     res.send()
   })
 })
-router.post('/api/admin/signin', urlencodedParser,function (req, res, next) {
-  //console.log(req.body);
-  req.body.password = md5(md5(req.body.password))
-  res.send()
+router.post('/api/admin/updateArticle', urlencodedParser, function (req, res, next) {
+  let info = req.body.articleInformation
+  Db.Article.find({_id: req.body.id}, function (err, data) {
+    if (err) {
+      return next(err)
+    }
+    data[0].title = info.title
+    data[0].date = info.date
+    data[0].content = info.content
+    data[0].gist = info.gist
+    data[0].labels = info.labels
+    Db.Article(data[0]).save(function (err) {
+      if (err) {
+        return next(err)
+      }
+      res.send()
+    })
+  })
 })
-router.get('/api/admin/getUser/:name', function (req, res) {
-  //console.log(req.query);
-  Db.User.findOne({
-    username: req.params.name
+/**
+ * 文章删除
+ */
+router.post('/api/admin/deleteArticle', urlencodedParser, function (req, res, next) {
+  Db.Article.remove({_id: req.body._id}, function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.send()
   })
 })
 module.exports = router;
