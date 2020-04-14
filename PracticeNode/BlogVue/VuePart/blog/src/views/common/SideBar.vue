a9e242b47c578a2c5d7c6c768aff620b8ce22e93
lgmayun <3496294573@qq.com>
2020-04-14 15:31:38 +0800

    美化页面

--- PracticeNode/BlogVue/VuePart/blog/src/views/common/SideBar.vue
+++ PracticeNode/BlogVue/VuePart/blog/src/views/common/SideBar.vue
@@ -8,31 +8,37 @@
       <ul>
         <router-link to="/article">
           <li class="nav">
+            <img src="~assets/img/SiderBar/首页.svg" class="router_picture">
             <span>首页</span>
           </li>
         </router-link>
         <router-link to="/about">
           <li class="nav">
+            <img src="~assets/img/SiderBar/关于.svg" class="router_picture">
             <span>关于</span>
           </li>
         </router-link>
         <router-link to="/tag">
           <li class="nav">
+            <img src="~assets/img/SiderBar/标签.svg" class="router_picture">
             <span>标签</span>
           </li>
         </router-link>
         <router-link to="/admin/signin">
           <li class="nav">
+            <img src="../../assets/img/SiderBar/登录.svg" class="router_picture">
             <span>登录</span>
           </li>
         </router-link>
         <router-link to="/admin/articleList">
           <li class="nav">
+            <img src="~assets/img/SiderBar/管理.svg" class="router_picture">
             <span>管理</span>
           </li>
         </router-link>
         <router-link to="/admin/articleEdit">
-          <li class="nav">
+          <li class="nav" >
+            <img src="~assets/img/SiderBar/写文章.svg" class="router_picture">
             <span>写文章</span>
           </li>
         </router-link>
@@ -54,22 +60,26 @@
       <ul class="communication">
         <li class="communication_item">
           <a href="#">
-            <span>GitHub</span>
+          <img src="../../assets/img/SiderBar/qq.svg" class="picture qq">
+            <span class="text">QQ</span>
           </a>
         </li>
         <li class="communication_item">
           <a href="#">
-            <span>GitHub</span>
+            <img src="~assets/img/SiderBar/github.svg" class="picture github">
+            <span class="text">GitHub</span>
           </a>
         </li>
         <li class="communication_item">
           <a href="#">
-            <span>GitHub</span>
+            <img src="~assets/img/SiderBar/twitter.svg" class="picture twitter">
+            <span class="text">Twitter</span>
           </a>
         </li>
         <li class="communication_item">
           <a href="#">
-            <span>GitHub</span>
+            <img src="~assets/img/SiderBar/facebook.svg" class="picture facebook">
+            <span class="text">Facebook</span>
           </a>
         </li>
       </ul>
@@ -99,5 +109,5 @@
 </script>
 
 <style scoped>
-  @import '../../assets/css/sidebar.css'
+  @import '../../assets/css/sidebar.css';
 </style>
